8b950890f93981e8746f3b18001d1a129296c332
naveenmm <naveen.mm@soti.net>
2020-03-02 09:51:31 +0530

    Changed flow

--- Music_oop/Scale.cs
+++ Music_oop/Scale.cs
@@ -9,27 +9,29 @@ namespace Music_oop
     {
         public string root { get; set; }//c,b
         public string[] notes { get; set; }//output
-        public string[] all_notes;//= { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        public string[] all_notes= new string[12]; //= { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
         //name :major,minor
         //key 2121121
         public Scale()
         {
-            all_notes = new string[12];
-            all_notes[0] = "C";
-            all_notes[1]= "C#";
-            all_notes[2]= "D";
-            all_notes[3]= "D#";
-            all_notes[4]= "E";
-            all_notes[5]= "F";
-            all_notes[6]= "F#";
-            all_notes[7]= "G";
-            all_notes[8]= "G#";
-            all_notes[9]= "A";
-            all_notes[10]= "A#";
-            all_notes[11]= "B";         
+            //all_notes = new string[12];            
+            get__notes();
             //root = Util.Console.Ask("Enter root:");
         }
-    
+
+        public void get__notes()
+        {
+            string line;
+            int i = 0;
+            System.IO.StreamReader r = new System.IO.StreamReader("notes.txt");
+            while ((line = r.ReadLine()) != null)
+            {                
+                    all_notes[i] = line;
+                i++;
+            }
+            r.Close();
+        }
+
         public string[] Compute(string val,int[] note)
         {
             int pos = 0;
@@ -47,6 +49,23 @@ namespace Music_oop
                     }
                     flag = 1;
                 }                
+            }
+            if(val=="ALL")
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    for (int k = 0; k < note.Length; k++)
+                    {
+                        a[k] = all_notes[(i + pos) % 12];
+                        pos = pos + note[k];
+                        if (i < 11)
+                        {
+                            Console.Write(a[k] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                flag = 1;
             }
             if(flag==0)
             {
